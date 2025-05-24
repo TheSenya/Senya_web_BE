83a78ffada9fd1d05757e32dec5849fb52b6908d
SenyaGithub <senyagithub@gmail.com>
2025-05-24 16:53:50 -0400

    alembic comment

--- start.prod.sh
+++ start.prod.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Wait for database to be ready
-echo "Waiting for database to be ready..."
-sleep 10
+# echo "Waiting for database to be ready..."
+# sleep 10
 
 # Run database migrations
-echo "Running database migrations..."
-alembic upgrade head
+# echo "Running database migrations..."
+# alembic upgrade head
 
 # Start the application
 echo "Starting application in production mode..."
